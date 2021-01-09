93fb1ab9fe50b798b5c24ee2113beb8ff238bb16
Marcin Luczak <mluczak92@gmail.com>
2021-01-09 09:04:09 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace tcp_server {
+    class Program {
+        static int port = 2121;
+
+        static void Main(string[] args) {
+            Log($"Hello world! I'm waiting for TCP packets on port {port}");
+        }
+
+        static void Log(string msg) {
+            Console.WriteLine($"{DateTime.Now.ToString("HH:mm")}\t{msg}");
+        }
+    }
+}
