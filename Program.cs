9f64bc88b6e944844e937ac366a3ac7b424ddbef
Marcin Luczak <mluczak92@gmail.com>
2021-01-10 16:01:30 +0100

    docker changes

--- Program.cs
+++ Program.cs
@@ -4,22 +4,33 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace tcp_server {
     class Program {
         static readonly short port = 2121;
         static readonly byte bufferSize = 64; //not lower than 5! first 5 bytes shows whole message size
+
+        static readonly TcpListener server = new TcpListener(IPAddress.Loopback, port);
         static readonly Dictionary<Guid, dynamic> clients = new Dictionary<Guid, dynamic>();
-        static TcpListener server;
 
         static void Main(string[] args) {
-            Print($"Hello world! Waiting for new clients on TCP port {port}...");
-            server = new TcpListener(IPAddress.Loopback, port);
             server.Start();
+            Print($"Hello world! Waiting for new clients on TCP port {port}...");
+
+            Console.CancelKeyPress += (o, e) => {
+                Console.WriteLine("Exititing...");
+                foreach (KeyValuePair<Guid, dynamic> entry in clients) {
+                    entry.Value.stream.Close();
+                    entry.Value.client.Close();
+                }
+                Environment.Exit(0);
+            };
 
             while (true) {
                 WaitForNextClient(out Guid id, out TcpClient client, out NetworkStream stream);
-                ReadMessageAsync(id, client, stream); //dont await
+                ReadMessageAsync(id, client, stream); //dont await -> create new background thread 
             }
         }
 
@@ -31,7 +42,7 @@ namespace tcp_server {
             Print($"New client!\t{id}\tTotal: {clients.Count}");
         }
 
-        async static void ReadMessageAsync(Guid id, TcpClient client, NetworkStream stream) {
+        async static Task ReadMessageAsync(Guid id, TcpClient client, NetworkStream stream) {
             StringBuilder messageBuilder = new StringBuilder();
             byte[] receivedBuffor = new byte[bufferSize];
             short sizeFromMessageHeader = 0;
@@ -57,7 +68,6 @@ namespace tcp_server {
                         messageBuilder.Clear();
                         sizeFromMessageHeader = 0;
                         alreadyReadBytes = 0;
-                        Print("Idle...");
                     }
                 }
             } catch (IOException) { //client force closed while connection was active
